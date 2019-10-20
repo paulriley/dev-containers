824e80f7b3dcaa939125e766ade7cdccd3f9d235
Paul Riley <paul@wsnhsolutions.com>
2019-10-20 21:11:44 +0000

    Rewrite pull-image

--- .github/actions/pull_image/pull-image.js
+++ .github/actions/pull_image/pull-image.js
@@ -4,17 +4,31 @@ const Docker = require('dockerode');
 try {
   const docker = new Docker({socketPath: '/var/run/docker.sock'});
   const dockerHubUser = core.getInput('docker-hub-user');
-  const image = docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`, err, stream => {
-    docker.modem.followProgress(stream, onFinished, onProgress);
-   
-    function onFinished(err, output) {
-      core.setOutput(output);
-    }
-
-    function onProgress(event) {
-      core.info(event)
-    }
-    });
+  const image = await docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`, processStream);
+  core.info(`Image pulled ${image.id}`);
+  core.setOutput(image.id);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function processStream(error, stream) {
+  if (error) {
+    core.info(err);
+    return;
+  }
+
+  docker.modem.followProgress(stream, onFinished, onProgress);
+
+  function onFinished(err, output) {
+    if (err) {
+      core.setFailed(err);
+    }
+    else {
+      core.info(output);
+    }
+  }
+  
+  function onProgress(event) {
+    core.info(event);
+  }
 }
