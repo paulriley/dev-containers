0b7d86579dd038d00990c9c6b6100659d9996e82
Paul Riley <paul@wsnhsolutions.com>
2019-10-20 21:45:08 +0000

    Use on hooks for stream

--- .github/actions/pull_image/pull-image.js
+++ .github/actions/pull_image/pull-image.js
@@ -4,31 +4,18 @@ const Docker = require('dockerode');
 try {
   const docker = new Docker({socketPath: '/var/run/docker.sock'});
   const dockerHubUser = core.getInput('docker-hub-user');
-  const image = docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`, (error, stream) => processStream(docker, error, stream));
+  const image = docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`, processStream);
   core.info(`Image pulled ${image.id}`);
   core.setOutput(image.id);
 } catch (error) {
   core.setFailed(error.message);
 }
 
-function processStream(docker, error, stream) {
+function processStream(error, stream) {
   if (error) {
     core.error(err);
-    return;
-  }
-
-  docker.modem.followProgress(stream, onFinished, onProgress);
-
-  function onFinished(err, output) {
-    if (err) {
-      core.setFailed(err);
-    }
-    else {
-      core.info(output);
-    }
-  }
-  
-  function onProgress(event) {
-    core.info(event);
+  } else {
+    stream.on('data', data => core.info(data));
+    stream.on('error', err => core.setFailed(err));
   }
 }
