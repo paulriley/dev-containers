0be5061bfb1ecdc6501d24a0a762b23cfac19b0b
Paul Riley <paul@wsnhsolutions.com>
2019-10-20 21:30:04 +0000

    Pass docker to process stream

--- .github/actions/pull_image/pull-image.js
+++ .github/actions/pull_image/pull-image.js
@@ -4,16 +4,16 @@ const Docker = require('dockerode');
 try {
   const docker = new Docker({socketPath: '/var/run/docker.sock'});
   const dockerHubUser = core.getInput('docker-hub-user');
-  const image = docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`, processStream);
+  const image = docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`, (error, stream) => processStream(docker, error, stream));
   core.info(`Image pulled ${image.id}`);
   core.setOutput(image.id);
 } catch (error) {
   core.setFailed(error.message);
 }
 
-function processStream(error, stream) {
+function processStream(docker, error, stream) {
   if (error) {
-    core.info(err);
+    core.error(err);
     return;
   }
 
