6b6e885c4e81782d851ed2d8ed3065336625588e
Paul Riley <paul@wsnhsolutions.com>
2019-10-20 19:16:34 +0000

    Add logging

--- .github/actions/pull_image/pull-image.js
+++ .github/actions/pull_image/pull-image.js
@@ -4,8 +4,17 @@ const Docker = require('dockerode');
 try {
   const docker = new Docker({socketPath: '/var/run/docker.sock'});
   const dockerHubUser = core.getInput('docker-hub-user');
-  const image = docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`);
-  core.setOutput('image', image);
+  const image = docker.pull(`${dockerHubUser}/vscode-dotnet-essentials:3.0`, err, stream => {
+    docker.modem.followProgress(stream, onFinished, onProgress);
+   
+    function onFinished(err, output) {
+      core.setOutput(output);
+    }
+
+    function onProgress(event) {
+      core.info(event)
+    }
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
