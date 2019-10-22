0b962e469427472f217db144078750bd21c7d7f9
Paul Riley <paul@wsnhsolutions.com>
2019-10-22 13:12:33 +0000

    Put a label on that ID

--- .github/actions/pull_image/pull-image.js
+++ .github/actions/pull_image/pull-image.js
@@ -7,7 +7,7 @@ try {
   const version = '3.0';
 
   pullImage(imageName, version)
-    .then(image => core.info(image.Id))
+    .then(image => core.info(`Image downloaded: ${image.Id}`))
     .catch(err => core.error(err));
 } catch (error) {
   core.setFailed(error.message);
